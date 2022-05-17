8293592a12fbea71511c5cff86357f162bd059b5
estydaven <esetnod1111@gmail.com>
2022-05-17 14:33:58 +0300

    add function of change portfolio images

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
+// Adaptive menu
+
 const menu = document.querySelector('.nav');
 const burger = document.querySelector('.burger');
 
-function openMenu() {
+function toggleMenu() {
     menu.classList.toggle('nav_active');
     burger.classList.toggle('burger_active');
 }
 
-burger.addEventListener('click', openMenu);
-menu.addEventListener('click', openMenu);
+burger.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
+
+// Change portfolio photos
+
+const portfolioBtns = document.querySelector('.portfolio__buttons');
+const portfolioImages = document.querySelectorAll('.photo');
+
+function changeImage(e) {
+    if (e.target.classList.contains('portfolio__button')) {
+        portfolioImages.forEach((img, index) => img.src = `images/${e.target.dataset.season}/${index + 1}.jpg`);
+    }
+}
+
+portfolioBtns.addEventListener('click', changeImage);
