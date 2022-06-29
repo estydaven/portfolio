08aa6e3d5abf525360c3a5033851bef22a726178
estydaven <esetnod1111@gmail.com>
2022-06-29 13:31:10 +0300

    add popups and scripts for them

--- js/main.js
+++ js/main.js
@@ -346,3 +346,41 @@ function scrollUp() {
 }
 
 window.addEventListener('scroll', scrollUp);
+
+// Popups
+
+// Popup hire
+const hireButton = document.querySelector('.button_hire');
+const hirePopup = document.querySelector('.popup_hire');
+const closeButtonHire = document.querySelector('.popup__close_hire');
+
+function showHirePopup() {
+    hirePopup.style.display = 'flex';
+    body.style.overflow = 'hidden';
+}
+function closeHirePopup() {
+    hirePopup.style.display = 'none';
+    body.style.overflow = 'auto';
+}
+
+hireButton.addEventListener('click', showHirePopup);
+closeButtonHire.addEventListener('click', closeHirePopup);
+
+// Popup price
+const priceButtons = document.querySelectorAll('[data-price]');
+const pricePopup = document.querySelector('.popup_price');
+const closeButtonPrice = document.querySelectorAll('.popup__close_price');
+const popupPrice = document.querySelector('.popup__price');
+
+function showPricePopup() {
+    popupPrice.innerText = this.dataset.price;
+    pricePopup.style.display = 'flex';
+    body.style.overflow = 'hidden';
+}
+function closePricePopup() {
+    pricePopup.style.display = 'none';
+    body.style.overflow = 'auto';
+}
+
+priceButtons.forEach(btn => btn.addEventListener('click', showPricePopup));
+closeButtonPrice.forEach(btnClose => btnClose.addEventListener('click', closePricePopup));
